2a0f24797f2313e32e5d4c725594e7a62c14ab1f
Anjana Hariprasad <anjanahariprasad21@gmail.com>
2025-03-20 11:47:50 +0100

    complete: Iteration 4

--- src/utils.ts
+++ src/utils.ts
@@ -26,11 +26,15 @@ countryElm.innerText= locationDetails.country;
 
 export function displayWeatherData(obj:WeatherResponse){
 const temperatureElm = document.getElementById('temperature') as HTMLElement;
-temperatureElm.innerText = 'Temperature : obj.current_weather.temperature + obj.current_weather_units.temperature ';
+const temperature = obj.current_weather.temperature + obj.current_weather_units.temperature; 
+temperatureElm.innerText = `Temperature : ${temperature}`;
 
 const windSpeedElm = document.getElementById('windspeed') as HTMLElement;
-windSpeedElm.innerText= 'Windspeed: obj.current_weather.windspeed + obj.current_weather_units.windspeed';
+const windspeed = obj.current_weather.windspeed + obj.current_weather_units.windspeed ;
+windSpeedElm.innerText= `Windspeed: ${windspeed}`;
 
 const windElm = document.getElementById('winddirection') as HTMLElement;
-windElm.innerText = 'Winddirection: obj.current_weather.winddirection+obj.current_weather_units.winddirection';
+const winddirection = obj.current_weather.winddirection+obj.current_weather_units.winddirection;
+
+windElm.innerText = `Winddirection: ${winddirection}`;
 }
